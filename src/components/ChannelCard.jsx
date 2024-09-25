e7f1af6cadd407738acafaa3d3cb82684996ced4
kebeattie <kebeattie98@hotmail.com>
2024-09-25 11:27:25 +0100

    Added search functionality

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ( { channelDetail }) => {
+const ChannelCard = ( { channelDetail, marginTop }) => {
   console.log(channelDetail);
   
   return (
@@ -19,6 +19,7 @@ const ChannelCard = ( { channelDetail }) => {
           width: {xs: '356px', md: '320px'},
           height: '326px',
           margin: 'auto',
+          marginTop: marginTop
           
         }}
       >
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromApi';
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <div>
+
+      <Box minHeight='95vh'>
+        <Box>
+          <div style={{
+            background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(56,166,189,1) 100%)',
+            zIndex: 10,
+            height: '300px'
+          }}
+          />
+          <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+        </Box>
+        <Box display="flex" p="2">
+
+          <Box sx={{ mr: { sm: '100px' } }} />
+          <Videos videos={videos}></Videos>
+        </Box>
+
+
+      </Box>
+
+    </div>
   )
 }
 
